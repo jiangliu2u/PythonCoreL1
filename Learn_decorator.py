da85733ffb77ef9ff09de5d1aee4549651ef01d0
jiangliu2u <jiangliu2u@gmail.com>
2017-12-11 18:36:30 +0800

    模拟登陆知乎

--- Learn_decorator.py
+++ Learn_decorator.py
@@ -1,33 +1,37 @@
 import time
 from functools import wraps
 
+
 def timethis(func):
-	'''
-	Decorator that reports the execution time.
-	'''
-	@wraps(func)
-	def wrapper(*args, **kwargs):
-		start = time.time()
-		result = func(*args, **kwargs)
-		end = time.time()
-		print(func.__name__, end-start)
-		return result
-	return wrapper
+    '''
+    Decorator that reports the execution time.
+    '''
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end - start)
+        return result
+
+    return wrapper
+
 
 def main():
+    @timethis
+    def countdown(n):
+        '''
+        Counts down
+        '''
+        while n > 0:
+            n -= 1
 
-	@timethis
-	def countdown(n):
-		'''
-		Counts down
-		'''
-		while n > 0:
-			n -= 1
-	countdown(1000000)
-	print(countdown.__name__)
-	print(countdown.__doc__)
-	print(countdown.__annotations__)
+    countdown(1000000)
+    print(countdown.__name__)
+    print(countdown.__doc__)
+    print(countdown.__annotations__)
 
 
 if __name__ == '__main__':
-	main()
+    main()
