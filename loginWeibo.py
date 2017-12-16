a4e1c472236a8207e74d303fd3c1e79e3a87b516
jiangliu2u <jiangliu2u@gmail.com>
2017-12-17 01:27:27 +0800

    爬取页面向下滚动自动加载的微博

--- loginWeibo.py
+++ loginWeibo.py
@@ -5,6 +5,7 @@ import time
 from PIL import Image
 import json
 import sys
+import pymongo
 
 # 构造 Request headers
 agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
@@ -34,25 +35,30 @@ def isLogin():
         return False
 
 
+weibo_3pages = []
+
+
 def getWeibo():
-    follow = session.get("https://m.weibo.cn/feed/friends?&max_id=4185694539281878", headers={
+    api = []
+    head = {
         'Accept': 'application/json, text/plain, */*',
         'Referer': 'https://m.weibo.cn/beta',
         'X-Requested-With': 'XMLHttpRequest',
         'User-Agent': agent
-    })
-
-    data = follow.json()['data']['statuses']
-    f=open('d:/2.json','a')
-    f.write(str(data))
-    for i in data:
-        b = i['text'].translate(non_bmp_map)
-        c = re.sub(r'<a .*?>', '', b)
-        c = re.sub(r'<span .*?</span>', '', c)
-        c = re.sub(r'</a>', '', c)
-        c = re.sub(r'<br/>', '', c)
-        print(c)
-        print('===================================')
+    }
+    url = "https://m.weibo.cn/feed/friends?&max_id="
+    follow = session.get(url, headers=head)
+    for i in range(6):
+        data = follow.json()['data']['statuses']
+        next_api = data[19]['mid']  # 第20条微博的mid为下拉页面加载更多微博的参数 请求链接为'https://m.weibo.cn/feed/friends?&max_id='+next_api
+        for i in data:
+            b = i['text'].translate(non_bmp_map)
+            c = re.sub(r'<a .*?>', '', b)
+            c = re.sub(r'<span .*?</span>', '', c)
+            c = re.sub(r'</a>', '', c)
+            c = re.sub(r'<br/>', '', c)
+            weibo_3pages.append(c)
+        follow = session.get(url + next_api, headers=head)
 
 
 def login(username, password):
@@ -81,6 +87,11 @@ def main():
         secret = '***'
         login(account, secret)
     getWeibo()
+    connection = pymongo.MongoClient('127.0.0.1', 27017)#把爬到的微博内容写入mongodb
+    weibo = connection.weibo
+    weibo = weibo.weibo
+    for i in weibo_3pages:
+        weibo.insert({'weibo_content': i})
 
 
 if __name__ == '__main__':
