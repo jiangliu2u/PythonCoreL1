cfdb7c3eb8db02a2a5eda35d2c181278ff186f0a
jiangliu2u <jiangliu2u@gmail.com>
2018-01-24 20:29:21 +0800

    decorator

--- decorator6.py
+++ decorator6.py
@@ -12,3 +12,24 @@ def print_full_name(first_name, last_name):
 
 
 print_full_name('Liu', 'Jiang')
+
+
+def method_friendly_decorator(method_to_decorate):
+    def wrapper(self, lie):
+        lie = lie - 3  # 女性福音 :-)
+        return method_to_decorate(self, lie)
+
+    return wrapper
+
+
+class Lucy(object):
+    def __init__(self):
+        self.age = 32
+
+    @method_friendly_decorator
+    def sayYourAge(self, lie):
+        print("I am {}, what did you think?".format(self.age+lie))
+
+
+l = Lucy()
+l.sayYourAge(-3)
